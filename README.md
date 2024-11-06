84ca52bee90d7eef54418e05703837c5a7882010
Kousuke Okabe <kousuke.okabe@ieee.org>
2024-11-06 00:31:48 +0000

    Create turtle_subscribe

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # turtle_sample
+
+# 実行
+    ターミナル１
+    $ roscore
+
+    ターミナル２
+    $ rosrun turtlesim turtlesim_node
+
+    ターミナル３
+    $ rosrus (パッケージ名) (ノード名)
+
+# パッケージ確認順序
+    1. turtle_publish
+    2. turtle_subscribe
+
+# パッケージ
+1. パッケージ作成
+    $ catkin_create_pkc (パッケージ名)
+
+2. フォルダ構造
+    >パッケージ
+        >config
+            >(パッケージ名).yaml
+        >launch
+            >(パッケージ名).launch
+        >src
+            >(パッケージ名).cpp
+        >CMakeLists.txt
+        >package.xml
--- turtle_subscribe/src/turtle_subscribe.cpp
+++ turtle_subscribe/src/turtle_subscribe.cpp
@@ -1,5 +1,19 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+
+#define K_pose  0.1
+#define ref_x   3.0
+#define ref_y   3.0
+
+double pose_x = 0.0;
+double pose_y = 0.0;
+
+// Subscribe時に実行する関数(Callback関数)
+void subfuncPose(const turtlesim::Pose &pose){
+    pose_x = pose.x;
+    pose_y = pose.y;
+}
 
 int main(int argc, char **argv){
     // ROSノードの初期化
@@ -10,23 +24,33 @@ int main(int argc, char **argv){
     // 送信するTopic通信(Publisher)の登録
     ros::Publisher pub_cmd = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
 
+    // 受信するTopic通信(Subscriber)の登録
+    ros::Subscriber sub_pos = nh.subscribe("/turtle1/pose", 10,subfuncPose);
+
     // ROSループ周期の設定
-    ros::Rate loop_rate(0.2);   // [Hz]
+    ros::Rate loop_rate(10);    // [Hz]
 
     // cmd_velのデータ型に対応した変数の宣言
     // https://docs.ros.org/en/noetic/api/geometry_msgs/html/msg/Twist.html
     geometry_msgs::Twist cmd;
-    cmd.linear.x = 1.0;
+    cmd.linear.x = 0.0;
     cmd.linear.y = 0.0;
     cmd.linear.z = 0.0;
     cmd.angular.x = 0.0;
     cmd.angular.y = 0.0;
-    cmd.angular.z = 1.0;
+    cmd.angular.z = 0.0;
 
     // ループ処理　ros::ok()は処理中断信号が来ていないかの確認
     while(ros::ok()){
+        // Topic通信の受信（Callback）の確認
+        ros::spinOnce();
+
         // Topic通信送信
         pub_cmd.publish(cmd);
+
+        // 比例制御
+        cmd.linear.x = K_pose * (ref_x  - pose_x);
+        cmd.linear.y = K_pose * (ref_y  - pose_y);
 
         // 設定したループ周期までスリープ
         loop_rate.sleep();
